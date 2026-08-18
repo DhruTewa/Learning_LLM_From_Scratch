0faedbe935c5398b1bd92e9a2fc352890eb1bdfa
Dhruv Tewari <dhrutewa@gmail.com>
2026-08-18 12:54:31 -0500

    GPT Architecture code upated

--- Foundation_Concepts/LLM_Design_Stage_1/GPT_Architecture.py
+++ Foundation_Concepts/LLM_Design_Stage_1/GPT_Architecture.py
@@ -1,5 +1,17 @@
 import torch
 import torch.nn as nn
+import tiktoken
+
+
+GPT_CONFIG_124M = {
+"vocab_size": 50257, # Vocabulary size
+"context_length": 1024, # Context length
+"emb_dim": 768, # Embedding dimension
+"n_heads": 12, # Number of attention heads
+"n_layers": 12, # Number of layers
+"drop_rate": 0.1, # Dropout rate
+"qkv_bias": False # Query-Key-Value bias
+}
 
 class DummyGPTModel(nn.Module):
     def __init__(self, cfg):
@@ -37,3 +49,18 @@ class DummyLayerNorm(nn.Module):
         super().__init__()
     def forward(self, x):
         return x
+    
+tokenizer = tiktoken.get_encoding("gpt2")
+batch = []
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch = torch.stack(batch,dim = 0)
+print(batch)
+
+torch.manual_seed(123)
+model = DummyGPTModel(GPT_CONFIG_124M)
+logits = model(batch)
+print("Output Shape :" ,logits.shape)
+print(logits)
